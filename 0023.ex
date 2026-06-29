a19e3bea5e110bae079d0e328187459495f160b7
June Rohrbaugh <jrohrbaugh@rideprt.org>
2026-06-29 14:44:08 -0400

    Solution to problem 23

---
+++ 0023.ex
@@ -0,0 +1,17 @@
+defmodule Euler0023 do
+	def divisor_sum(n) do
+		(for x <- 1..trunc(n ** 0.5), rem(n, x) == 0, do: (if x * x == n or x == 1 do x else x + (div(n, x)) end)) |> Enum.sum()
+	end
+	
+	def abundant?(n) do
+		divisor_sum(n) > n
+	end
+	
+	def non_abundant_sums() do
+		abundants = for n <- 1..28123, abundant?(n), do: n
+		abundant_sums = for x <- abundants, y <- abundants, y >= x, x + y <= 28123, into: MapSet.new(), do: x + y
+		(for n <- 1..28123, !Enum.member?(abundant_sums, n), do: n) |> Enum.sum()
+	end
+end
+
+IO.puts(Euler0023.non_abundant_sums())
