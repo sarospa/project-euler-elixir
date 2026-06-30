18e4019b5bc96e2a355ba2de5003c5c51369bfac
June Rohrbaugh <jrohrbaugh@rideprt.org>
2026-06-30 09:07:22 -0400

    Solutions to problems 24 and 25

---
+++ 0024.ex
@@ -0,0 +1,13 @@
+defmodule Euler0024 do
+	def find_lex_permutation(list, target) do
+		if length(list) == 1 do
+			Enum.at(list, 0)
+		else
+			count = (for n <- 1..(length(list) - 1), do: n) |> Enum.product()
+			term = Enum.at(list, div(target, count))
+			term * (10 ** (length(list) - 1)) + find_lex_permutation(List.delete(list, term), rem(target, count))
+		end
+	end
+end
+
+IO.puts(Euler0024.find_lex_permutation((for n <- 0..9, do: n), 999999))
---
+++ 0025.ex
@@ -0,0 +1,11 @@
+defmodule Euler0025 do
+	def fibonacci_index(a, b, i) do
+		if b >= 10 ** 999 do
+			i
+		else
+			fibonacci_index(b, a + b, i + 1)
+		end
+	end
+end
+
+IO.puts(Euler0025.fibonacci_index(1, 1, 2))
