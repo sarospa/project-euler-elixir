c684fe7fff85e7bac3724146f27ffa927d3bd65b
June Rohrbaugh <jrohrbaugh@rideprt.org>
2026-07-17 13:10:26 -0400

    Solution to problem 59

---
+++ 0059.ex
@@ -0,0 +1,15 @@
+# Solution to https://projecteuler.net/problem=59
+
+defmodule Euler0059 do
+	def decrypt_with(ciphertext, key) do
+		Enum.zip_with(ciphertext, Stream.cycle(key), fn x, y -> Bitwise.bxor(x, y) end)
+	end
+
+	def decrypt() do
+		ciphertext = File.read!("0059_cipher.txt") |> String.split(",") |> Enum.map(&String.to_integer/1)
+		decrypts = for a <- 97..122, b <- 97..122, c <- 97..122, do: decrypt_with(ciphertext, [a, b, c])
+		Enum.max_by(decrypts, fn list -> Enum.count(list, &(&1 == 32)) end) |> Enum.sum()
+	end
+end
+
+IO.inspect(Euler0059.decrypt(), charlists: :as_charlists)
