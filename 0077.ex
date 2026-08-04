988c99d2758feac0e33273e881711ede8c01b89a
June Rohrbaugh <jrohrbaugh@rideprt.org>
2026-08-04 14:52:11 -0400

    Solution to problem 77

---
+++ 0077.ex
@@ -0,0 +1,23 @@
+# Solution to https://projecteuler.net/problem=75
+
+Code.require_file("primes.ex")
+
+defmodule Euler0077 do
+	def count_prime_sums(n, total, bound) do
+		if total == n do
+			1
+		else
+			(for m <- 1..bound, Primes.prime?(m), do: count_prime_sums(n, total + m, Enum.min([m, n - (total + m)]))) |> Enum.sum()
+		end
+	end
+	
+	def find_prime_sums(n \\ 10) do
+		if count_prime_sums(n, 0, n - 1) > 5000 do
+			n
+		else
+			find_prime_sums(n + 1)
+		end
+	end
+end
+
+IO.puts(Euler0077.find_prime_sums())
